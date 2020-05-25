cd40be792f307c82c16ca9548f754356e1c83eaa
Devon (PC) <reece.devon@gmail.com>
2020-05-26 01:38:21 +1200

    Started some work to add a map to page. Little css

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-
 function ZipMap(props) {
+    var map;
+    var mapScrip = document.createElement('script');
+    mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+    mapScrip.defer = true;
+    mapScrip.async = true;
+
+    window.initMap = function() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+            }
+        )};
+    document.head.appendChild(mapScrip);
+
     return (
-        <h1>test</h1>
+        <div className="map">
+            <style jsx="true">{`
+                        .map{
+                        height:100%;
+                        }
+                    `}
+            </style>
+        </div>
     )
 }
 
