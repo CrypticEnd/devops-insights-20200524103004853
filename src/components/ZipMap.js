18275027bdba270b704bf59593dd7fe1d6f4aefd
Devon (PC) <reece.devon@gmail.com>
2020-05-26 01:42:09 +1200

    build failed took out map code to see if issue was with renaming vars

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 function ZipMap(props) {
-    var map;
-    var mapScrip = document.createElement('script');
-    mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
-    mapScrip.defer = true;
-    mapScrip.async = true;
-
-    window.initMap = function() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-            }
-        )};
-    document.head.appendChild(mapScrip);
+    // var map;
+    // var mapScrip = document.createElement('script');
+    // mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+    // mapScrip.defer = true;
+    // mapScrip.async = true;
+    //
+    // window.initMap = function() {
+    //     map = new google.maps.Map(document.getElementById('map'), {
+    //         center: {lat: -34.397, lng: 150.644},
+    //         zoom: 8
+    //         }
+    //     )};
+    // document.head.appendChild(mapScrip);
 
     return (
         <div className="map">
