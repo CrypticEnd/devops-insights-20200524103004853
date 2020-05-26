effd4b6b8a78810926aab5b2b01cf820c4702afc
Devon (PC) <reece.devon@gmail.com>
2020-05-26 19:22:34 +1200

    everthing broke commented out stuff to see if it fixes

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -2,20 +2,20 @@ import React from 'react';
 import google from 'googlemaps';
 
 function ZipMap(props) {
-    var map;
-    const mapScrip = document.createElement('script');
-    mapScrip.type = 'text/javascript';
-    mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
-    mapScrip.defer = true;
-    mapScrip.async = true;
-    document.head.appendChild(mapScrip);
-
-    function intiMap() {
-        map = new google.maps.Map(document.getElementsByClassName('map'), {
-            center: {lat: -39.824778, lng: 174.983245},
-            zoom: 5
-        });
-    }
+    // var map;
+    // const mapScrip = document.createElement('script');
+    // mapScrip.type = 'text/javascript';
+    // mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
+    // mapScrip.defer = true;
+    // mapScrip.async = true;
+    // document.head.appendChild(mapScrip);
+    //
+    // function intiMap() {
+    //     map = new google.maps.Map(document.getElementsByClassName('map'), {
+    //         center: {lat: -39.824778, lng: 174.983245},
+    //         zoom: 5
+    //     });
+    // }
 
     //  function initMap() {
     //     map = new google.maps.Map(document.getElementById('map'), {
@@ -26,7 +26,8 @@ function ZipMap(props) {
 
 
     return (
-        <div ref="map" style="height: '500px', width: '500px'" onLoad={intiMap()}>
+        <div ref="map" style="height: '500px', width: '500px'" >
+            test
         </div>
     )
 }
