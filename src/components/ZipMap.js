d0c030784958fd9fb46eb2cc2bc3e1216cf60e10
Devon (PC) <reece.devon@gmail.com>
2020-05-26 16:49:26 +1200

    Trying to get the map to compile

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -1,22 +1,29 @@
 import React from 'react';
 
 function ZipMap(props) {
-    // var map;
-    // var mapScrip = document.createElement('script');
-    // mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
-    // mapScrip.defer = true;
-    // mapScrip.async = true;
-    //
-    // window.initMap = function() {
+    var map;
+    const mapScrip = document.createElement('script');
+    mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
+    mapScrip.defer = true;
+    mapScrip.async = true;
+
+    function intiMap() {
+        new google.maps.Map(document.getElementsByClassName('map'), {
+            center: {lat: -39.824778, lng: 174.983245},
+            zoom: 7
+        });
+    }
+
+    //  function initMap() {
     //     map = new google.maps.Map(document.getElementById('map'), {
     //         center: {lat: -34.397, lng: 150.644},
     //         zoom: 8
     //         }
-    //     )};
+    //     )}
     // document.head.appendChild(mapScrip);
 
     return (
-        <div className="map">
+        <div className="map" onLoad={intiMap()}>
             <style jsx="true">{`
                         .map{
                         height:100%;
