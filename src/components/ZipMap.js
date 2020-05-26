d261a30ab2ad766a94ec29984fe8662c7e9371f3
Devon (PC) <reece.devon@gmail.com>
2020-05-27 01:25:17 +1200

    testing out map inplment again. (forgot a semi colon)

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function ZipMap(props) {
     var map;
-    const google = window.google;
     const mapScrip = document.createElement('script');
     mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
     mapScrip.defer = true;
     mapScrip.async = true;
     document.head.appendChild(mapScrip);
+    const google = window.google;
 
     window.onloadend(function () {
         map = new google.maps.Map(document.getElementsByClassName('map'), {
