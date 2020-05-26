5e82fd4a722dda666f21117487c51741d3c4959c
Devon (PC) <reece.devon@gmail.com>
2020-05-27 01:30:06 +1200

    testing out map inplment again. trying to fix onload function

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 function ZipMap(props) {
     var map;
+    const google = window.google = window.google ? window.google : {};
     const mapScrip = document.createElement('script');
     mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
     mapScrip.defer = true;
     mapScrip.async = true;
     document.head.appendChild(mapScrip);
-    const google = window.google;
 
-    window.onloadend(function () {
+
+    window.onload(function () {
         map = new google.maps.Map(document.getElementsByClassName('map'), {
             center: {lat: -39.824778, lng: 174.983245},
             zoom: 7
