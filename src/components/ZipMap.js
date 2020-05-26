a75a4e8a79178e055cc117daeafa42f78423d0a6
Devon (PC) <reece.devon@gmail.com>
2020-05-27 00:05:02 +1200

    testing out map inplment again

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 function ZipMap(props) {
     var map;
-    // const mapScrip = document.createElement('script');
-    // mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
-    // mapScrip.defer = true;
-    // mapScrip.async = true;
+    const mapScrip = document.createElement('script');
+    mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
+    mapScrip.defer = true;
+    mapScrip.async = true;
 
-    window.initMap = function () {
+    window.onload(function () {
         map = new google.maps.Map(document.getElementsByClassName('map'), {
             center: {lat: -39.824778, lng: 174.983245},
             zoom: 7
         });
-    }
+    });
+
     document.head.appendChild(mapScrip);
 
     return (
