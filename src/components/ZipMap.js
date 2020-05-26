94f224d924a9184b36e5fcfe36712fcfbe1f7260
Devon (PC) <reece.devon@gmail.com>
2020-05-26 18:52:34 +1200

    Installed googlemaps plugin maybe

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import google from 'googlemaps';
 
 function ZipMap(props) {
     var map;
     const mapScrip = document.createElement('script');
     mapScrip.type = 'text/javascript';
     mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
-    // mapScrip.defer = true;
-    // mapScrip.async = true;
+    mapScrip.defer = true;
+    mapScrip.async = true;
     document.head.appendChild(mapScrip);
 
     function intiMap() {
-        new google.maps.Map(document.getElementsByClassName('map'), {
+        map = new google.maps.Map(document.getElementsByClassName('map'), {
             center: {lat: -39.824778, lng: 174.983245},
-            zoom: 7
+            zoom: 5
         });
     }
 
