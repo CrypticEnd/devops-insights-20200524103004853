5f85f6dda8e3013105ab5e47cffec4c17a6ed55b
Devon (PC) <reece.devon@gmail.com>
2020-05-26 20:04:36 +1200

    reset to before i install googled map plugins

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import google from 'googlemaps';
 
 function ZipMap(props) {
-    // var map;
-    // const mapScrip = document.createElement('script');
-    // mapScrip.type = 'text/javascript';
-    // mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
+    var map;
+    const mapScrip = document.createElement('script');
+    mapScrip.type = 'text/javascript';
+    mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
     // mapScrip.defer = true;
     // mapScrip.async = true;
-    // document.head.appendChild(mapScrip);
-    //
-    // function intiMap() {
-    //     map = new google.maps.Map(document.getElementsByClassName('map'), {
-    //         center: {lat: -39.824778, lng: 174.983245},
-    //         zoom: 5
-    //     });
-    // }
+    document.head.appendChild(mapScrip);
+
+    function intiMap() {
+        // new google.maps.Map(document.getElementsByClassName('map'), {
+        //     center: {lat: -39.824778, lng: 174.983245},
+        //     zoom: 7
+        // });
+    }
 
     //  function initMap() {
     //     map = new google.maps.Map(document.getElementById('map'), {
@@ -26,7 +25,7 @@ function ZipMap(props) {
 
 
     return (
-        <div ref="map" style="height: '500px', width: '500px'" >
+        <div ref="map" style="height: '500px', width: '500px'" onLoad={intiMap()}>
             test
         </div>
     )
