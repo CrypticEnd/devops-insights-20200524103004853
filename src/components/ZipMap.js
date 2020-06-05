ca5e40dcdd5acd08ed65c22ceffa73a2cdf82d77
Devon (PC) <reece.devon@gmail.com>
2020-06-05 14:03:20 +1200

    needing a screen shot

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ZipMap(props) {
     var map;
-    const google = window.google = window.google ? window.google : {};
+    //const google = window.google = window.google ? window.google : {};
     const mapScrip = document.createElement('script');
     mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
     mapScrip.defer = true;
