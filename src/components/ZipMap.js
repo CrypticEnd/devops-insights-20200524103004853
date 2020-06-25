4c0b3135a1be3cb2cb21dbe4539796082c673017
Devon (PC) <reece.devon@gmail.com>
2020-06-25 16:28:24 +1200

    "working" stage one for handin (re-uploading to make sure webpage is vaild)

--- src/components/ZipMap.js
+++ src/components/ZipMap.js
@@ -1,25 +1,25 @@
-import React from 'react';
-
-function ZipMap(props) {
-    var map;
-    const google = window.google = window.google ? window.google : {};
-    const mapScrip = document.createElement('script');
-    mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
-    mapScrip.defer = true;
-    mapScrip.async = true;
-    document.head.appendChild(mapScrip);
-
-
-    function intiMap() {
-        // map = new google.maps.Map(document.getElementsByClassName('map'), {
-        //     center: {lat: -39.824778, lng: 174.983245},
-        //     zoom: 7
-        // });
-    }
-
-    return (
-        <div className="map" onLoad={intiMap()}></div>
-    )
-}
-
-export default ZipMap
+// import React from 'react';
+//
+// function ZipMap(props) {
+//     var map;
+//     const google = window.google = window.google ? window.google : {};
+//     const mapScrip = document.createElement('script');
+//     mapScrip.src = 'https://maps.googleapis.com/maps/api/js?key=6b7b471967dd0851d0010cdecf28f829&callback=initMap';
+//     mapScrip.defer = true;
+//     mapScrip.async = true;
+//     document.head.appendChild(mapScrip);
+//
+//
+//     function intiMap() {
+//         map = new google.maps.Map(document.getElementsByClassName('map'), {
+//             center: {lat: -39.824778, lng: 174.983245},
+//             zoom: 7
+//         });
+//     }
+//
+//     return (
+//         <div className="map" onLoad={intiMap()}></div>
+//     )
+// }
+//
+// export default ZipMap
